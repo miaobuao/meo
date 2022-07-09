7062e855607ab9913ab3cbba192d26d247a63af3
miaobuao <617802189@qq.com>
2022-07-10 04:02:26 +0800

    Chang load & dump function name

--- meo/__version__.py
+++ meo/__version__.py
@@ -3,9 +3,9 @@ package declaration
 """
 __title__ = 'meo'
 __description__ = 'frequently-used function library'
-__version__ = '0.0.1.2002.7.10'
+__version__ = '0.0.1'
 __url__ = "http://github.com/miaobuao/meo"
 __author__ = 'miaobuao'
 __author_email__ = '617802189@qq.com'
-__license__ = 'MIT'
+__license__ = 'MIT License'
 __copyright__ = 'Copyright 2022 miaobuao'
