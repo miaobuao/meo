bb9637c53a18b7becd6367bc3944c841956683f6
miaobuao <miaobuao@outlook.com>
2023-06-26 15:08:58 +0800

    docs: update crack-zip

--- examples/crack-zip.py
+++ examples/crack-zip.py
@@ -1,8 +1,17 @@
 from meo.crack import Crack
 from meo.utils import script_path
+import meo
 import os
 
 path = os.path.join(script_path(__file__), "./data/flag.zip") # ./flag.zip
 zguess = Crack.zip(path)
-pwd = zguess.in_multiprocess_by_seed(progressbar=True)
-print(pwd)
+
+t, pwd = meo.wapper.timeit(
+    lambda: zguess.in_multiprocess_by_seed()
+)()
+print(f"use {t}s -> {pwd}")
+
+t, pwd = meo.wapper.timeit(
+    lambda: zguess.by_seed()
+)()
+print(f"use {t}s -> {pwd}")
