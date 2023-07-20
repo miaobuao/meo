6393054453f034803bbb73aede6363ccfa3ed507
miaobuao <miaobuao@outlook.com>
2023-07-20 20:15:03 +0800

    feat: add `SimplePath`

--- meo/utils.py
+++ meo/utils.py
@@ -6,3 +6,14 @@ import os
 def script_path(__file__):
     """return script real path in disk."""
     return os.path.split(os.path.realpath(__file__))[0]
+
+class SimplePath:
+    def __init__(self, path: str) -> None:
+        self.path = path
+
+    def join(self, *paths):
+        path = os.path.join(self.path, *paths)
+        return SimplePath(path)
+
+def ScriptPath(__file__):
+    return SimplePath(script_path(__file__))
